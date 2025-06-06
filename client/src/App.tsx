879d8a807e98cc111e98f2049736ab7d76cfdbe3
sarang <saranggp2018@gmail.com>
2025-06-06 23:31:40 +0530

    setup payment app

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+  <div>
+    <Button>Use this </Button>
+  </div>
+    </>
+  )
+}
+
+export default App
